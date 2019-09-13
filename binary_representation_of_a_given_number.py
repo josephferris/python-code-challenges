bcd366863f25a999742065b8c5ce3d7020526ed2
Joey Ferris <joey@joeyferris.io>
2019-09-13 15:25:47 -0500

    Initial commit

---
+++ binary_representation_of_a_given_number.py
@@ -0,0 +1,30 @@
+#
+# Method 1: Iterative
+# For any number, we can check whether its ‘i’th bit is 0(OFF) or 1(ON) by bitwise ANDing it with “2^i” (2 raise to i).
+#
+# 1) Let us take number 'NUM' and we want to check whether it's 0th bit is ON or OFF
+#     bit = 2 ^ 0 (0th bit)
+#     if  NUM & bit == 1 means 0th bit is ON else 0th bit is OFF
+#
+# 2) Similarly if we want to check whether 5th bit is ON or OFF
+#     bit = 2 ^ 5 (5th bit)
+#     if NUM & bit == 1 means its 5th bit is ON else 5th bit is OFF.
+#
+#
+# Method 2: Recursive
+# Following is recursive method to print binary representation of ‘NUM’.
+#
+# step 1) if NUM > 1
+#     a) push NUM on stack
+#     b) recursively call function with 'NUM / 2'
+# step 2)
+#     a) pop NUM from stack, divide it by 2 and print it's remainder.
+#
+#
+# Method 3: Recursive using bitwise operator
+# Steps to convert decimal number to its binary representation are given below:
+
+# step 1: Check n > 0
+# step 2: Right shift the number by 1 bit and recursive function call
+# step 3: Print the bits of number
+#
